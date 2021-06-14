4b1d6e06113c544ce056e8fa67e6b733eaff4615
Ennio Sousa <ennio@enniosousa.com.br>
2021-06-13 21:01:17 -0300

    Modify stubs

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,3 +1,17 @@
-<!-- DataTable Bootstrap -->
-<link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<!-- DataTable Bootstrap 4 -->
+<style>
+div.dataTables_wrapper div.dataTables_filter {
+  text-align: right;
+}
+div.dataTables_wrapper div.dataTables_filter label {
+  font-weight: normal;
+  white-space: nowrap;
+  text-align: left;
+}
+div.dataTables_wrapper div.dataTables_filter input {
+  margin-left: 0.5em;
+  display: inline-block;
+  width: auto;
+}
+</style>
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.bootstrap4.min.css">
